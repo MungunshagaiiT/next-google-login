b4a929e48a33d87df6f3f3b3138737dc581d5062
MungunshagaiiT <mungunshagai.t@pinecone.mn>
2023-04-13 13:49:47 +0800

    google login added

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const index = () => {
+  const handleClick = () => {};
+  return (
+    <div className="align-center text-center">
+      <button onClick={handleClick} className="bg-blue ">
+        Google login
+      </button>
+    </div>
+  );
+};
+
+export default index;
